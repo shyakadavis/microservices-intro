fe6b2e39b457368d71dbb8f3bb13c54a14906943
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2023-05-12 14:38:27 +0200

    🪜 ch: add cors

--- comments/src/express/express.config.ts
+++ comments/src/express/express.config.ts
@@ -1,10 +1,12 @@
 import express, { Application } from 'express';
+import cors from 'cors';
 import commentsRouter from './routes';
 
 const ExpressConfig = (): Application => {
   const app = express();
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+  app.use(cors());
   app.get('/', (req, res) => {
     res.json('🐒 Hello from the comments service');
   });
--- posts/src/express/express.config.ts
+++ posts/src/express/express.config.ts
@@ -1,10 +1,12 @@
 import express, { Application } from 'express';
+import cors from 'cors';
 import postsRouter from './routes';
 
 const ExpressConfig = (): Application => {
   const app = express();
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+  app.use(cors());
   app.get('/', (req, res) => {
     res.json('🐒 Hello from the posts feature');
   });
