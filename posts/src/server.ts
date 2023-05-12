ea9654c71c3d651d587fc13c81010263dab2b007
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2023-05-12 22:29:28 +0200

    ⛴️ update ports

--- posts/src/server.ts
+++ posts/src/server.ts
@@ -2,6 +2,6 @@ import ExpressConfig from './express/express.config';
 import 'dotenv/config';
 
 const app = ExpressConfig();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log('🍏 Server Running on Port ' + PORT));
