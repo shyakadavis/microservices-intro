3f56a49a1b44557eee5ebf48bf994d807b5d4076
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2023-05-14 19:31:01 +0200

    ✅ testing communication

--- event-bus/src/controllers/index.ts
+++ event-bus/src/controllers/index.ts
@@ -11,15 +11,15 @@ export const handleEvent = (req: Request, res: Response) => {
   axios.post('http://posts-clusterip-srv:8080/events', event).catch((err) => {
     console.error(err.message);
   });
-  // axios.post('http://localhost:8081/events', event).catch((err) => {
-  //   console.error(err.message);
-  // });
-  // axios.post('http://localhost:8082/events', event).catch((err) => {
-  //   console.error(err.message);
-  // });
-  // axios.post('http://localhost:8083/events', event).catch((err) => {
-  //   console.error(err.message);
-  // });
+  axios.post('http://comments-srv:8081/events', event).catch((err) => {
+    console.error(err.message);
+  });
+  axios.post('http://query-srv:8082/events', event).catch((err) => {
+    console.error(err.message);
+  });
+  axios.post('http://moderation-srv:8083/events', event).catch((err) => {
+    console.error(err.message);
+  });
 
   res.send({ status: 'OK' });
 };
